cb70a053c296377e9a117a0274e49f9070ff4ae1
PRERANA <preranac03@gmail.com>
2024-12-07 21:35:18 +0530

    Add files via upload

---
+++ VolumeOfCylinder.java
@@ -0,0 +1,11 @@
+
+//volume of cylinder => pi*r*r*h;
+public class VolumeOfCylinder {
+    public static void main (String []args){
+  int r = 2;
+  int h = 4;
+  double volume = Math.PI*r*r*h;
+  System.out.println("The volume of Cylinder is "+ volume);
+
+    }
+}
