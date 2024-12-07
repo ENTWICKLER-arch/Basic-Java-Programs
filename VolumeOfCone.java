463cf3ca893c05356e914f51403735e6bbcd9a0e
PRERANA <preranac03@gmail.com>
2024-12-07 21:25:13 +0530

    Add files via upload

---
+++ VolumeOfCone.java
@@ -0,0 +1,10 @@
+//volume of cone => 1/3*pi*r*r*h;
+
+public class VolumeOfCone {
+ public static void main (String[] args){
+int r = 3;
+int h =4;
+double volume = (1.0/3.0)*Math.PI*r*r*h;
+System.out.println("The volume of cone is"+volume);
+}
+}
