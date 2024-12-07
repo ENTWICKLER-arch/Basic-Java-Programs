6a2eb3bf8a4eab281774857b076a59777fdc4da8
PRERANA <preranac03@gmail.com>
2024-12-07 20:05:52 +0530

    Update and rename Hello.java to HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+// Program to print Hello World in Java (8:04 pm , Saturday !!!when i again try to start coding again by scratch ..lets follow this routine for a long time 
+//date is 7 december 2024)
+
+
+public class Hello {
+public static void main(String[] args) {
+    System.out.println("Hello,World");
+}
+
+}
