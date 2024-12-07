4e7a6735d9deba19ca777ce44901119954f456e2
PRERANA <preranac03@gmail.com>
2024-12-07 21:35:44 +0530

    Add files via upload

---
+++ VolumeOfCube.java
@@ -0,0 +1,11 @@
+//volume of cube => r*r*r;
+
+
+public class VolumeOfCube {
+    public static void main (String []args){
+        int r= 5;
+        int volume = r*r*r;
+        System.out.println("Volume of cone is "+volume);
+    }
+    
+}
