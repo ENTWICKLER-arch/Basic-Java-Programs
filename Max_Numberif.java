472ce47b8a286a922fca18ec641fe51350a7df11
PRERANA <preranac03@gmail.com>
2024-12-08 21:25:21 +0530

    Add files via upload
    Taking input from user and printing the maximum number..

---
+++ Max_Numberif.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+public class Max_Numberif {
+public static void main(String[]args){
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter num 1 = ");
+int num1= sc.nextInt();
+System.out.print("Enter num 2 = ");
+int num2 = sc.nextInt();
+if (num1>num2){
+    System.out.println(num1);
+}
+else{
+    System.out.println(num2);
+}
+}
+    
+}
