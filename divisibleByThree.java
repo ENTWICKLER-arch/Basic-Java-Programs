0ab3f068b8849c145929d9013a68c119b509830f
PRERANA <preranac03@gmail.com>
2024-12-08 21:58:20 +0530

    Add files via upload
    Code to determine input number by user is divisible or not by 3

---
+++ divisibleByThree.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+public class divisibleByThree {
+    public static void main (String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter num = ");
+    int num = sc. nextInt();
+    if (num%3==0){
+        System.out.println("Number is divisible by 3");
+    } 
+    else{
+        System.out.println("Not divisible by 3");
+    }
+    }
+}
